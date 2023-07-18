d4db71811e3269abe837e0ba5cc6dfe749ddddde
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-07-18 15:16:49 +0330

    some more costumized

--- components/elements/Task.js
+++ components/elements/Task.js
@@ -2,9 +2,13 @@ import axios from "axios";
 import styles from "./Task.module.css";
 import { VscChevronLeft, VscChevronRight } from "react-icons/vsc";
 import Toast from "./Toast";
+import { useEffect, useState } from "react";
 
 function Task({ title, status, description, _id, fetch }) {
+  const [details, setDetails] = useState(false);
+
   const statuses = ["new", "in progress", "review", "done"];
+
   const index = statuses.findIndex((s) => s === status);
 
   const handler = async (id, status) => {
@@ -26,30 +30,55 @@ function Task({ title, status, description, _id, fetch }) {
       <div className={styles.title}>
         {title}
         <div className={styles.status}>{status}</div>
+        <label
+          htmlFor="menu"
+          className={details ? `${styles.menu} ${styles.active}` : styles.menu}
+          onClick={() => setDetails((prevState) => !prevState)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
       </div>
-      <div className={styles.description}>{description}</div>
-      <span className={styles.line}></span>
-      <div className={styles.buttons}>
-        {index === 0 ? (
-          <span></span>
-        ) : (
-          <button
-            className={styles.backButton}
-            onClick={() => handler(_id, statuses[index - 1], title)}
-          >
-            <VscChevronLeft /> {statuses[index - 1]}
-          </button>
-        )}
-        {index === statuses.length - 1 ? (
-          <span></span>
-        ) : (
-          <button
-            className={styles.nextButton}
-            onClick={() => handler(_id, statuses[index + 1], title)}
-          >
-            {statuses[index + 1]} <VscChevronRight />
-          </button>
-        )}
+
+      <div
+        className={
+          details ? `${styles.dropDown} ${styles.active}` : styles.dropDown
+        }
+      >
+        {/* <div className={styles.description}> */}
+        <div
+          className={
+            !details
+              ? styles.description
+              : `${styles.description} ${styles.active}`
+          }
+        >
+          {description}
+        </div>
+        <span className={styles.line}></span>
+        <div className={styles.buttons}>
+          {index === 0 ? (
+            <span></span>
+          ) : (
+            <button
+              className={styles.backButton}
+              onClick={() => handler(_id, statuses[index - 1], title)}
+            >
+              <VscChevronLeft /> {statuses[index - 1]}
+            </button>
+          )}
+          {index === statuses.length - 1 ? (
+            <span></span>
+          ) : (
+            <button
+              className={styles.nextButton}
+              onClick={() => handler(_id, statuses[index + 1], title)}
+            >
+              {statuses[index + 1]} <VscChevronRight />
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
